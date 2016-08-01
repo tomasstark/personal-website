bc949f4458542ff31e4a6e6fd4b248950cf6c935
Tomas Stark <tom@tomstark.me>
2016-08-01 21:04:41 +0200

    copy for about page

--- app/components/about.js
+++ app/components/about.js
@@ -4,6 +4,8 @@ import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 const Header = require('./bootstrap/header');
 const Footer = require('./bootstrap/footer');
 
+let copy = require('../data/copy.json');
+
 class About extends React.Component {
     constructor(props) {
         super(props);
@@ -16,7 +18,17 @@ class About extends React.Component {
 
                 <section className="static">
                     <div className="container">
-                        <div className="grid"></div>
+                        <section>
+                            <h1>{copy.about.about_me.title}</h1>
+
+                            <div dangerouslySetInnerHTML={{__html:copy.about.about_me.text}}></div>
+                        </section>
+
+                        <section>
+                            <h1>{copy.about.tools.title}</h1>
+
+                            <div dangerouslySetInnerHTML={{__html: copy.about.tools.text}}></div>
+                        </section>
                     </div>
                 </section>
 
