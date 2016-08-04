efffad1b7e3fd22bb47e749a72bad0f4a13dfb63
Tomas Stark <tom@tomstark.me>
2016-08-04 16:54:19 +0200

    fixing copy

--- react/components/about.js
+++ react/components/about.js
@@ -24,13 +24,13 @@ class About extends React.Component {
 
                                 <h1>About me</h1>
 
-                                <p>My name is Tomas Stark and people usually <em>call me Tom</em>. I am a freelance developer who specializes in turning beautifully designed user interfaces into working product. I have been working with UI designers very closely during my career and I am applying knowledge gained through years of this collaboration to clients projects every day.</p>
+                                <p>My name is Tomas Stark and people usually <em>call me Tom</em>. I am a freelance developer who specializes in front-end, turning designs into final product using cutting edge technologies. I have been working with teams of UX and UI designers very closely during my career and I am applying knowledge gained through years of this collaboration to clients’ projects every day.</p>
 
-                    <p>My primary focus is doing front end development using the most popular technologies such as <em>React, Redux, Node.js</em>, or making crazy (or normal) <em>CSS stuff</em>. That is it for the buzz words section :-). <em>WordPress</em> is my passion too and you can find some examples to prove it in <Link to="/">Work</Link> section of this website. I developed a lot of custom themes and plugins, and I have been working with the WordPress platform from <em>version 2.0</em>.</p>
+                                <p>My primary focus is front end development using the most popular technologies such as <em>React, Redux, Node.js</em>, making crazy (or normal) <em>CSS stuff</em>, or developing complex <em>WordPress</em> themes and plugins. I have been working with the WordPress platform from <em>version 2.0</em>. So much for the buzz words section :-)</p>
 
-                                <p>Writing the code isn't always the difficult part of the job. But writing the code that is both beautiful and efficient can be really challenging. If you are an individual or a company looking for a new pair of eyes to go through your code and highlight those weak spots of your application, <Link to="/contact">let's talk about it</Link>. I did a lot of reviews in past and was working as a consultant too.</p>
+                                <p>Writing the code that is both beautiful and efficient can be really challenging most of the time and it is very clever to have a fresh pair of eyes look into your code. If you are an individual or a company looking for somebody to highlight those weak spots of your application, <Link to="/contact">let's talk about it</Link>. I did a lot of reviews in the past and was working as a consultant for both startups and corporations.</p>
 
-                                <p>I did not always freelance. I worked couple of agency jobs before, the most recent one being Tribal Worldwide where I was a senior front end developer and a leader of local development team for almost 2 years.</p>
+                                <p>I was not always a freelancer. I worked couple of agency jobs before for years, the most recent one being Tribal Worldwide where I was a senior front end developer and a leader of local development team for almost 2 years.</p>
 
                                 <p>Sounds convincing? I am currently available for new projects. <Link to="/contact">Hire me!</Link></p>
                             </section>
